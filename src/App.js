17ae5c291640c07a5fd6f708f12a93e8a1cabc8d
fengshuo <fengshuo142003@gmail.com>
2016-08-28 11:37:59 -0400

    react with animation branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      imgs: [
+        'https://unsplash.it/458/354?random&time=1472179609076',
+        'https://unsplash.it/458/354?random&time=1472179634185',
+        'https://unsplash.it/458/354?random&time=1472179639144',
+        'https://unsplash.it/458/354?random&time=1472179649184',
+        'https://unsplash.it/458/354?random&time=1472179659091'
+      ],
+      currentIndex: 0
+    }
+
+    this.navPrev = this.navPrev.bind(this);
+    this.navNext = this.navNext.bind(this);
+
+  }
+
+  navNext() {
+    let state = this.state;
+    let current = state.currentIndex;
+    let imgLength = state.imgs.length;
+    this.setState({
+      currentIndex: current < imgLength - 1 ? current + 1 : 0
+    })
+  }
+
+  navPrev() {
+    let state = this.state;
+    let current = state.currentIndex;
+    let imgLength = state.imgs.length;
+    this.setState({
+      currentIndex: current <= 0 ? imgLength - 1: current - 1
+    })
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
-    );
+      <div className="carousel">
+        <div className="carousel_img">
+          <ReactCSSTransitionGroup transitionName="carousel" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
+            <img src={this.state.imgs[this.state.currentIndex]} key={this.state.imgs[this.state.currentIndex]} />
+          </ReactCSSTransitionGroup>
+        </div>
+        <div className="carousel_btns">
+          <button onClick={this.navPrev}>Prev</button>
+          <button onClick={this.navNext}>Next</button>
+        </div>
+      </div>
+    )
   }
 }
